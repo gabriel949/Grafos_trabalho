8f9ba4cdd9049ae7456912dba6c3be9c2a44316a
gabriel949 <gabriel.costa949@gmail.com>
2017-11-20 16:01:11 -0200

    trabalho

---
+++ grafo/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace grafo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int vertice = 0, vertice1;
+
+
+            Console.SetWindowSize(Console.WindowWidth + 45, Console.WindowHeight + 10);
+
+            while (vertice == 0)
+            {
+                try
+                {
+                    Console.Write("Digite o número de vértices : ");
+                    vertice = int.Parse(Console.ReadLine());
+
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("\n\nDigite uma opção válida!!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+            grafos matriz = new grafos();
+            matriz.grafo(vertice);
+
+
+
+            int cont = 0;
+            int tamanho = 0;
+            int opcao = 0;
+            string[] cidades = new string[] {"Betim","Belo Horizonte","Caeté","Contagem","Ibirité","Lagoa Santa","Nova Lima","Pedro Leopoldo",
+            "Raposos","Ribeirão das Neves","Rio Acima","Sabará","Santa Luzia","Vespasiano","Brumadinho","Esmeraldas","Igarapé","Mateus Leme","Arcos","Governardor Valadares"};
+
+
+            while (opcao != 7)
+            {
+                if (cont == 1)
+                {
+                    vertice = tamanho;
+                }
+                Console.Clear();
+                Console.WriteLine("1 - Criar arestas.");
+                Console.WriteLine("2 - Remover aresta.");
+                Console.WriteLine("3 - Reiniciar grafo.");
+                Console.WriteLine("4 - Mostrar Grafo.");
+                Console.WriteLine("5 - Busca em largura");
+                Console.WriteLine("6 - Ler arquivo txt");
+                Console.WriteLine("7 - Sair");
+                try
+                {
+                    Console.Write("Escolha uma opção : ");
+                    opcao = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("\n\nDigite uma opção válida!!");
+                    opcao = 0;
+                    Console.ReadKey();
+                }
+
+                switch (opcao)
+                {
+
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("\n Cidades e seus respectivos vértices :");
+
+                        for (int i = 1; i <= vertice; i++)
+                        {
+                            Console.WriteLine("\n {0} = Vértice {1}", cidades[i - 1], i);
+                        }
+                        Console.WriteLine("\n Pressione enter para continuar!");
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine(" Digite as cidades que serão adjacentes entre sí: ");
+                        Console.WriteLine(" ================================================");
+
+                        //                        try
+                        // {
+                        Console.Write("\n\n Vertice 1 : ");
+                        vertice1 = int.Parse(Console.ReadLine());
+                        //  }
+                        // catch (System.FormatException)
+                        // {
+                        Console.WriteLine("Digite um vértice existente!!");
+                        // }
+                        //try { 
+                        Console.Write("\n\n Vertice 2 : ");
+                        int vertice2 = int.Parse(Console.ReadLine());
+
+                        //  }
+
+                        //  catch (System.FormatException)
+                        //{
+                        Console.WriteLine("Digite um vértice existente!!");
+                        //   }
+                        matriz.arestas(vertice1, vertice2);
+
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("\n Cidades e seus respectivos vértices :");
+
+                        for (int i = 1; i <= vertice; i++)
+                        {
+                            Console.WriteLine("\n {0} = Vértice {1}", cidades[i - 1], i);
+                        }
+                        Console.WriteLine("\n Pressione enter para continuar!");
+                        Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine(" Digite as vertices que terão arestas removidas");
+                        Console.WriteLine(" ==============================================");
+                        Console.Write("\n\n Vertice 1 : ");
+                        int verticeRemover1 = int.Parse(Console.ReadLine());
+                        Console.Write("\n\n Vertice 2 : ");
+                        int verticeRemover2 = int.Parse(Console.ReadLine());
+
+
+                        matriz.removerAresstas(verticeRemover1, verticeRemover2);
+
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        matriz.Reiniciar(vertice);
+                        break;
+
+                    case 4:
+                        Console.Clear();
+                        matriz.mostrar(vertice);
+                        break;
+
+                    case 5:
+                        matriz.Busca(vertice, cidades);
+                        break;
+
+                    case 6:
+                        Console.Clear();
+                        Console.Write(" Essa opcão irá criar um novo grafo de acordo com os parametros passados no arquivo txt.\n\n Pressione S para continuar ou N para cancelar: ");
+                        string escolha = Console.ReadLine();
+
+                         if (escolha.ToUpper() == "N")
+                         {
+                             break;
+                         }
+                         if(escolha.ToUpper()=="S")
+                         {
+                            cont++;
+                            string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), @"hhhh.txt");
+                            string text = System.IO.File.ReadAllText(path);
+                            string[] lines = text.Split('\n');
+                            string[] aux = new string[lines.Length];
+                            tamanho = int.Parse(lines[0]);
+
+                            matriz.grafo(tamanho);
+
+
+                            for (int i =0;i<lines.Length-3; i++)
+                            {
+                                aux[i] = lines[i+1];
+                                string[] x = aux[i].Split(';');
+                                matriz.arestas(Convert.ToInt32(x[0]), Convert.ToInt32(x[1]));
+
+                            }
+
+                        }
+
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Opção Inválida!!!");
+                        }
+
+                        Console.ReadKey();
+
+                        break;
+
+                }
+
+            }
+
+
+
+        }
+    }
+
+
+}
---
+++ grafo/grafos.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace grafo
+{
+    class grafos
+    {
+        public int[,] matriz;
+
+        public void grafo(int tamanho)
+        {
+            matriz = new int[tamanho, tamanho];
+        }
+
+        public void arestas(int a, int b)
+        {
+            matriz[a - 1, b - 1] = 1;
+            matriz[b - 1, a - 1] = 1;
+
+        }
+
+        public void removerAresstas(int a, int b)
+        {
+            matriz[a - 1, b - 1] = 0;
+            matriz[b - 1, a - 1] = 0;
+        }
+
+        public void mostrar(int a)
+        {
+            int cont = 0;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            for (int i = 1; i < a + 1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\t " + i);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < a; i++)
+            {
+                cont++;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.Write(cont);
+
+
+                for (int j = 0; j < a; j++)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (matriz[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.Write("\t " + matriz[i, j]);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+
+        public void Reiniciar(int a)
+        {
+            int cont = 0;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            for (int i = 1; i < a + 1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("\t " + i);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < a; i++)
+            {
+                cont++;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(cont);
+
+                for (int j = 0; j < a; j++)
+                {
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    if (matriz[i, j] == 1)
+                    {
+                        matriz[i, j] = 0;
+
+                    }
+                    Console.Write("\t " + matriz[i, j]);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public void LerArquivo()
+        {
+            int cont = 0;
+            string text = System.IO.File.ReadAllText(@"C:\Users\depau\Desktop\Softwares\grafo\hhhh.txt");
+            string[] lines = text.Split(';');
+            foreach(string x in lines)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+        }
+
+        public void Busca(int vertic,string[]cidade)
+        {
+            Console.Clear();
+            Console.WriteLine("\n Cidades e seus respectivos vértices :");
+
+            for (int i = 1; i <= vertic; i++)
+            {
+               Console.WriteLine("\n {0} = Vértice {1}", cidade[i - 1], i);
+            }
+
+            Console.Write("\n\n\n Digite o vértice da cidade de início :" );
+            int indexCidade = int.Parse(Console.ReadLine());
+
+            Console.Write("\n\n\n Digite o vértice da cidade a ser pesquisada :");
+            int cidadePesquisa = int.Parse(Console.ReadLine());
+
+            Queue v = new Queue();
+            vertice[] z = new vertice[vertic];
+            vertice numer;
+            Queue q = new Queue();
+            for (int i = 0; i < vertic; i++)
+            {
+                numer = new vertice();
+                numer.numero = i;
+                z[i] = numer;
+            }
+
+            vertice raiz = z[indexCidade-1];
+            raiz.visitado = true;
+            q.Enqueue(raiz.numero);
+            int b = cidadePesquisa-1 ;
+
+            int cont = 0;
+            while (q.Count > 0)
+            {
+                if (cont == 1)
+                {
+                    break;
+                }
+                for (int j = 0; j < vertic; j++)
+                {
+                    int i = Convert.ToInt32(q.Peek());
+                    if (matriz[i, j] == 1)
+                    {
+                        if (z[j].numero == b)
+                        {
+                            q.Enqueue(z[j].numero);
+                            Console.Clear();
+                            Console.WriteLine("Foi encontrado o vertíce pesquisado");
+                            Console.WriteLine("\n Pressione enter para ver as cidades que a busca passou para encontrar {0}",cidade[cidadePesquisa-1]);
+                            Console.ReadKey();
+
+                            cont++;
+                            break;
+                        }
+                        if (z[j].visitado == false)
+                        {
+                            z[j].visitado = true;
+                            q.Enqueue(z[j].numero);
+                        }
+                    }
+
+                }
+
+                v.Enqueue(q.Dequeue());
+            }
+
+            while (q.Count > 0)
+            {
+                v.Enqueue(q.Dequeue());
+            }
+            string[] a = new string[10];
+            Console.Clear();
+            if (cont > 0)
+            {
+                foreach (int c in v)
+                {
+                    if (c == b)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    
+                    Console.WriteLine(cidade[c]);
+                    
+                    a[cont] = cidade[c];
+                  
+                    Console.ForegroundColor = ConsoleColor.White;
+                    cont++;
+                }
+            }
+          
+            else
+            {
+                Console.WriteLine("Não foi encontrado o vértice pesquisado,verifique se é um grafo conexo");
+            }
+            string path = Path.Combine(Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory), "out.xt");
+            System.IO.File.WriteAllLines(path, a);
+            Console.ReadLine();
+        }
+
+
+        class vertice
+        {
+
+            public bool visitado { get; set; }
+            public int numero { get; set; }
+
+        }
+    }
+}
+
+
+
+
